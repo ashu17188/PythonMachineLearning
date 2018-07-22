9e0170f1073a2641c66246c25f89d06e5ae4cfe1
shuklaas <ashutosh.shukla@adp.com>
2018-07-22 16:19:46 +0530

    numpy tutorial commit

---
+++ NumPy.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 13:37:29 2018
+NumPy, which stands for Numerical Python, is a library consisting of 
+multidimensional array objects and a collection of routines for processing 
+those arrays. Using NumPy, mathematical and logical operations on arrays 
+can be performed. 
+"""
+
+adfa
