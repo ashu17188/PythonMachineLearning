fa564f78a3f4c3c9e5e40afafe070c3a62efc672
Ashutosh Shukla <ashu17188@gmail.com>
2019-10-28 18:26:03 +0530

    Further changes

--- DataVisualisation/test.py
+++ DataVisualisation/test.py
@@ -6,17 +6,47 @@ Created on Sun Sep 29 17:07:37 2019
 @author: administrator
 """
 
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from collections import Counter
-from subprocess import check_output
-import numpy as np
+import os
 
-#print(check_output(["ls", "./datasets"]).decode("utf8"))
+#print(os.listdir('../datasets'))
+plt.figure(figsize=(12,7))
+sns.set(style='whitegrid')
+df = pd.read_csv('../datasets/StudentsPerformance.csv')
 
-kill = pd.read_csv('../datasets/PoliceKillingsUS.csv', encoding="windows-1252");
-#print(kill.info());
-#sns.countplot(x='age',data=kill);
-sns.barplot(x='age',y='city',data=kill)
+df.rename(columns=({'gender':'Gender','race/ethnicity':'Race/Ethnicity',
+                   'parental level of education':'Parental_Level_Of_Education',
+                   'lunch':'Lunch','test preparation course':'Test_Preparation_Course',
+                   'math score':'Math_Score','reading score':'Reading_Score',
+                   'writing score':'Writing_Score'}),inplace=True);
+
+print(df['Test_Preparation_Course'].unique());
+
+newDf =  df[df['Test_Preparation_Course']=='completed'].groupby(df['Lunch']).Writing_Score.sum();
+print(newDf)
+X=df.iloc[:,5]
+
+Y= df.iloc[:,0]
+
+h = df.iloc[:,2]
+
+plt.legend(loc=0)
+sns.barplot(x=newDf.index,y=newDf.values)
+plt.xticks(rotation=45)
+plt.title('Student performance report')
+#Completed upto 33 
+"""
+gender                         object
+race/ethnicity                 object
+parental level of education    object
+lunch                          object
+test preparation course        object
+math score                      int64
+reading score                   int64
+writing score                   int64
+"""
+
+
